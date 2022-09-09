9c85ff86d8f8e35d7cdfc940edd47eacc5fc41fa
Evan Voeltner <evanvoeltner@gmail.com>
2022-09-09 14:02:17 -0500

    feat: binary tree can determine if the node exists and who its parent and children are

--- binary_node.py
+++ binary_node.py
@@ -15,6 +15,7 @@ class BinaryTree:
             if current_node.value < node_value:
                 if current_node.greater_child == None:
                     current_node.greater_child = new_treenode
+                    current_node.greater_child.parent = current_node
                     node_placed = True
                     print(f'Greater child node created with a value of {node_value} for the parent {current_node.value}')
                 else:
@@ -23,15 +24,41 @@ class BinaryTree:
             elif current_node.value > node_value:
                 if current_node.lesser_child == None:
                     current_node.lesser_child = new_treenode
+                    current_node.lesser_child.parent = current_node
                     node_placed = True
                     print(f'Lesser child node created with a value of {node_value} for the parent {current_node.value}')
                 else:
                     current_node = current_node.lesser_child
                     
-        
+    def print_node_info(self, node):
+        lesser_child = ''
+        greater_child = ''
+        if node.lesser_child != None:
+            lesser_child =  f'| Lesser Child {node.lesser_child.value} '
+        if node.greater_child != None:
+            greater_child =  f'| Greater Child {node.greater_child.value} '
+        print(f'Value {node.value} | Parent {node.parent.value} ' + lesser_child + greater_child)
 
-    def search_for_node(self):
-        pass
+    def search_for_node(self, value_to_find):
+        current_node = self.root
+        while True:
+            if current_node.value == value_to_find:
+                print('Node Found')
+                self.print_node_info(current_node)
+                return 
+            elif current_node.value < value_to_find:
+                if current_node.greater_child == None:
+                    print(f"Node {value_to_find} doesn't exist")
+                    return 
+                else:
+                    current_node = current_node.greater_child
+            elif current_node.value > value_to_find:
+                if current_node.lesser_child == None:
+                    print(f"Node {value_to_find} doesn't exist")
+                    return
+                else:
+                    current_node = current_node.lesser_child
+    
 
 class TreeNode:
     def __init__(self, value):
